946af792a4949a14cdf21c8a354452f98eb6b88c
InCorCadit16 <incorcadit16@gmail.com>
2019-11-29 08:33:52 +0200

    add realization of parse method and add connection to DBUtils

--- src/main/java/com/technical/task/DBUtils.java
+++ src/main/java/com/technical/task/DBUtils.java
@@ -1,8 +1,23 @@
 package com.technical.task;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 
 public class DBUtils {
+    private static final String DB_NAME = "records.db";
+
+    private static Connection connect() {
+        Connection conn = null;
+        try {
+            String url = "jdbc:sqlite:src\\main\\resources\\" + DB_NAME;
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException dbe) {
+            System.out.println(dbe.getLocalizedMessage());
+        }
+        return conn;
+    }
 
     static void insertAll(List<Record> recordsList) {
 
--- src/main/java/com/technical/task/DataParser.java
+++ src/main/java/com/technical/task/DataParser.java
@@ -1,8 +1,40 @@
 package com.technical.task;
 
 public class DataParser {
+    private static final String QUOTE = ",";
 
     static Record parse(String data) {
-        return null;
+        Record record = new Record();
+
+        StringBuilder dataBuilder = new StringBuilder(data);
+
+        record.setA(getDataAndRemove(dataBuilder));
+        record.setB(getDataAndRemove(dataBuilder));
+        record.setC(getDataAndRemove(dataBuilder));
+        record.setD(getDataAndRemove(dataBuilder));
+
+        // parse base64 image separately
+        String s = dataBuilder.substring(0, dataBuilder.lastIndexOf("\"") + 1);
+        dataBuilder.delete(0, dataBuilder.lastIndexOf("\"") + 2);
+        record.setE(s);
+
+        record.setF(getDataAndRemove(dataBuilder));
+        record.setG(getDataAndRemove(dataBuilder));
+        record.setH(getDataAndRemove(dataBuilder));
+        record.setI(getDataAndRemove(dataBuilder));
+        record.setJ(getDataAndRemove(dataBuilder));
+        return record;
+}
+
+    private static String getDataAndRemove(StringBuilder dataBuilder) {
+        String s;
+        if (dataBuilder.indexOf(QUOTE) == -1) {
+            s = dataBuilder.toString();
+        } else {
+            s = dataBuilder.substring(0, dataBuilder.indexOf(QUOTE));
+            dataBuilder.delete(0, dataBuilder.indexOf(QUOTE) + 1);
+        }
+
+        return s;
     }
 }

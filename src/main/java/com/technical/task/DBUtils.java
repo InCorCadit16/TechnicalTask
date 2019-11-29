770c35d70f324fefb3448786a26abf2320713287
InCorCadit16 <incorcadit16@gmail.com>
2019-11-29 08:41:59 +0200

    add FileUtils methods realization and insertion of data to database

--- src/main/java/com/technical/task/DBUtils.java
+++ src/main/java/com/technical/task/DBUtils.java
@@ -2,16 +2,29 @@ package com.technical.task;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class DBUtils {
+    private static final int MAX_RECORDS_IN_QUERY = 900;
     private static final String DB_NAME = "records.db";
+    private static final String TABLE_NAME = "RECORDS";
+
+    private static final String CREATE_TABLE_QUERY = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" +
+                                                        "A VARCHAR," + " B VARCHAR," +
+                                                        "C VARCHAR," + " D VARCHAR," +
+                                                        "E VARCHAR," + " F VARCHAR," +
+                                                        "G VARCHAR," + " H VARCHAR," +
+                                                        "I VARCHAR," + " J VARCHAR " +
+                                                        ")";
+
+    private static final String INSERT_RECORD_QUERY = "INSERT INTO " + TABLE_NAME + " VALUES(?,?,?,?,?,?,?,?,?,?)";
 
     private static Connection connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite:src\\main\\resources\\" + DB_NAME;
+            String url = "jdbc:sqlite:" + FileUtils.WORK_DIR + DB_NAME;
             conn = DriverManager.getConnection(url);
         } catch (SQLException dbe) {
             System.out.println(dbe.getLocalizedMessage());
@@ -20,6 +33,45 @@ public class DBUtils {
     }
 
     static void insertAll(List<Record> recordsList) {
+        try (Connection connection = connect()) {
+            connection.setAutoCommit(false);
+            connection.prepareStatement(CREATE_TABLE_QUERY).execute();
 
+            try (PreparedStatement insertStatement = connection.prepareStatement(INSERT_RECORD_QUERY)) {
+
+                int recordsCount = 0;
+                for (Record record : recordsList) {
+                    addRecordToBatch(record, insertStatement);
+
+                    if (recordsCount == MAX_RECORDS_IN_QUERY) {
+                        insertStatement.executeBatch();
+                        connection.commit();
+                        recordsCount = 0;
+                    } else {
+                        recordsCount++;
+                    }
+                }
+
+                insertStatement.executeBatch();
+                connection.commit();
+            }
+            System.out.println("Data inserted in records.db");
+        } catch (SQLException se) {
+            System.out.println(se.getLocalizedMessage());
+        }
+    }
+
+    private static void addRecordToBatch(Record record, PreparedStatement insertStatement) throws SQLException {
+        insertStatement.setString(1,record.getA());
+        insertStatement.setString(2,record.getB());
+        insertStatement.setString(3,record.getC());
+        insertStatement.setString(4,record.getD());
+        insertStatement.setString(5,record.getE());
+        insertStatement.setString(6,record.getF());
+        insertStatement.setString(7,record.getG());
+        insertStatement.setString(8,record.getH());
+        insertStatement.setString(9,record.getI());
+        insertStatement.setString(10,record.getJ());
+        insertStatement.addBatch();
     }
 }
--- src/main/java/com/technical/task/FileUtils.java
+++ src/main/java/com/technical/task/FileUtils.java
@@ -1,19 +1,63 @@
 package com.technical.task;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileUtils {
+    static final String WORK_DIR = "src\\main\\resources\\";
+    private static final int BUFFER_ALLOC = 8192;
+    private static final String TIMESTAMP = "YYYY-mm-dd_hh-MM-ss";
 
     static LinkedList<String> readData() {
-        return null;
+        LinkedList<String> writings = new LinkedList<>();
+        Path path = Paths.get(WORK_DIR + "X.csv");
+        try (Scanner scanner = new Scanner(path)) {
+            // Miss first line with "A,B,C,D,E,F,G,H,I,J"
+            scanner.nextLine();
+            while (scanner.hasNextLine()) {
+                writings.add(scanner.nextLine());
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return writings;
     }
 
     static void submitErrorFile(List<Record> errors) {
-
+        String fileName = buildErrorFileName();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(WORK_DIR + fileName),BUFFER_ALLOC)) {
+            for (Record badRecord : errors) {
+                writer.write(badRecord.toString(),0,badRecord.toString().length());
+                writer.newLine();
+            }
+            System.out.println("Error data inserted in error file");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getLocalizedMessage());
+        }
     }
 
     static void submitStatisticsFile(int goodRecords, int badRecords) {
+        String fileName = "statistics.log";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(WORK_DIR + fileName),BUFFER_ALLOC)) {
+            writer.write(goodRecords + badRecords + " of records received\n");
+            writer.write(goodRecords + " of records successful\n");
+            writer.write(badRecords + " of records failed");
+            System.out.println("Log file filled up");
+        } catch (IOException ioe) {
+            System.out.println(ioe.getLocalizedMessage());
+        }
+    }
 
+    private static String buildErrorFileName() {
+        SimpleDateFormat format = new SimpleDateFormat(TIMESTAMP);
+        return "bad-data-" + format.format(new Date()) + ".csv";
     }
 }
